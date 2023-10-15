a314d21c54815e9910e9b008e0ac12eafca26417
Mateusz Krystek <mbkrystek@gmail.com>
2023-10-15 22:35:26 +0200

    AnimatePresence component was added

--- src/components/Dropdown/DropdownList.jsx
+++ src/components/Dropdown/DropdownList.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 const DropdownList = (props) => {
   const { sortByList, isOpen, handleTraitClick, sortBy } = props;
 
   return (
-    <>
+    <AnimatePresence>
       {isOpen && (
         <motion.div
           className="flex flex-col bg-gray-100 absolute items-end right-0 rounded-md whitespace-nowrap"
-          initial={{ opacity: 0, y: -5 }}
+          initial={{ opacity: 0, y: -20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 0.05 }}
+          exit={{ opacity: 0, y: -5 }}
         >
           {sortByList.map((trait) => (
             <div className="flex items-end px-2 py-0.5">
@@ -28,7 +29,7 @@ const DropdownList = (props) => {
           ))}
         </motion.div>
       )}
-    </>
+    </AnimatePresence>
   );
 };
 

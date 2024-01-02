4b2e010b2a5fb4c9590221383096965a3771fe5e
Mateusz Krystek <mbkrystek@gmail.com>
2024-01-02 21:50:08 +0100

    changed Products component/landing-page

--- src/components/NewestProducts/Product.jsx
+++ src/components/NewestProducts/Product.jsx
@@ -4,55 +4,29 @@ import RevealHorizontal from "../Animations/RevealHorizontal";
 
 const Product = ({ product, buttonText, leftSided }) => {
   return (
-    <section className="flex justify-between w-full h-96 items-stretch">
-      {leftSided ? (
-        <>
-          <div className="bg-amber-400 w-[30%]" />
-          <RevealHorizontal
-            moveLeft={true}
-            className="w-[70%] bg-white flex items-center justify-center  border-t-2"
-          >
-            <img
-              src={product.image}
-              alt={product.name}
-              className="h-full py-4"
-            />
-          </RevealHorizontal>
-          <RevealHorizontal
-            moveLeft={false}
-            className="absolute h-80 w-auto max-w-md bg-gray-200 left-[15%] self-center flex flex-col justify-between items-center py-10 rounded"
-          >
-            <div className="m-4 whitespace-normal text-center text-2xl">
-              {product.description}
-            </div>
-            <ButtonCTA>{buttonText}</ButtonCTA>
-          </RevealHorizontal>
-        </>
-      ) : (
-        <>
-          <RevealHorizontal
-            moveLeft={false}
-            className="w-[70%] bg-white flex items-center justify-center  border-t-2 relative"
-          >
-            <img
-              src={product.image}
-              alt={product.name}
-              className="h-full py-4"
-            />
-          </RevealHorizontal>
-          <div className="bg-amber-400 w-[30%]" />
-
-          <RevealHorizontal
-            moveLeft={true}
-            className="absolute h-80 w-auto max-w-md bg-gray-200 right-[15%] self-center flex flex-col justify-between items-center py-10 rounded"
-          >
-            <p className="m-4 whitespace-normal text-center text-2xl">
-              {product.description}
-            </p>
-            <ButtonCTA>{buttonText}</ButtonCTA>
-          </RevealHorizontal>
-        </>
-      )}
+    <section className="flex justify-between w-full h-96 items-stretch relative">
+      <div
+        className={`bg-amber-400 w-[30%] ${
+          leftSided ? "order-first" : "order-last"
+        }`}
+      />
+      <div className="bg-white w-[70%] border-t-2" />
+      <div className="flex justify-between absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 w-[70%] h-[90%]">
+        <RevealHorizontal
+          moveLeft={!leftSided}
+          className={`h-80 w-auto max-w-md bg-gray-200 self-center flex flex-col justify-between items-center py-10 rounded ${
+            leftSided ? "order-first" : "order-last"
+          }`}
+        >
+          <div className="m-4 whitespace-normal text-center text-2xl">
+            {product.description}
+          </div>
+          <ButtonCTA>{buttonText}</ButtonCTA>
+        </RevealHorizontal>
+        <RevealHorizontal moveLeft={leftSided}>
+          <img src={product.image} alt={product.name} className="h-full py-4" />
+        </RevealHorizontal>
+      </div>
     </section>
   );
 };

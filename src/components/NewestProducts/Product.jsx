57642daef3e1927c2f942812a38978a767c043df
Mateusz Krystek <mbkrystek@gmail.com>
2024-01-06 16:34:25 +0100

    changed Product component-feature/landing-page

--- src/components/NewestProducts/Product.jsx
+++ src/components/NewestProducts/Product.jsx
@@ -4,17 +4,17 @@ import RevealHorizontal from "../Animations/RevealHorizontal";
 
 const Product = ({ product, buttonText, leftSided }) => {
   return (
-    <section className="flex justify-between w-full h-96 items-stretch relative">
+    <section className="flex justify-between w-full h-auto items-stretch relative bg-white border-b-2 ">
       <div
-        className={`bg-amber-400 w-[30%] ${
-          leftSided ? "order-first" : "order-last"
+        className={`bg-amber-400 h-full w-[30%] absolute z-0  ${
+          leftSided ? "left-0" : "right-0"
         }`}
       />
-      <div className="bg-white w-[70%] border-t-2" />
-      <div className="flex justify-between absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 w-[70%] h-[90%]">
+
+      <div className="flex items-center w-[70%] justify-between z-10 mx-auto">
         <RevealHorizontal
           moveLeft={!leftSided}
-          className={`h-80 w-auto max-w-md bg-gray-200 self-center flex flex-col justify-between items-center py-10 rounded ${
+          className={`h-80 max-w-md bg-gray-200 flex flex-col justify-between items-center py-10 rounded ${
             leftSided ? "order-first" : "order-last"
           }`}
         >
@@ -24,7 +24,11 @@ const Product = ({ product, buttonText, leftSided }) => {
           <ButtonCTA>{buttonText}</ButtonCTA>
         </RevealHorizontal>
         <RevealHorizontal moveLeft={leftSided}>
-          <img src={product.image} alt={product.name} className="h-full py-4" />
+          <img
+            src={product.image}
+            alt={product.name}
+            className="h-auto max-h-96 w-full object-cover py-4"
+          />
         </RevealHorizontal>
       </div>
     </section>
